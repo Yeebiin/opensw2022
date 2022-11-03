c4f0717326cdc3d142ec9015968ef674e7b5f967
Yebin Kim <yeb2426@gmail.com>
2022-11-03 17:20:34 +0900

    zoo() is added

---
+++ include/myfuncs.c
@@ -0,0 +1,10 @@
+#include "myfuncs.h"
+#include <stdio.h>
+
+int main(){
+	void foo();
+	void goo();
+	void zoo();
+
+	return 0;
+}
---
+++ src/myfuncs.h
@@ -0,0 +1,13 @@
+#include "myfuncs.h"
+#include <stdio.h>
+
+void foo(void){
+	printf("hell foo\n");
+}
+void goo(void){
+	printf("hello goo\n");
+}
+void hoo(void){
+	printf("hello hoo\n");
+}
+
